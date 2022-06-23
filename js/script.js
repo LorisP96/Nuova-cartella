6560a2313e52adc81fb5ab4aa549b383e60555fc
LorisP96 <lorispalazzo96@gmail.com>
2022-06-23 13:06:41 +0200

    flanders BONUS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         helloWorld : 'Salve salvino amico vicino',
-        helloColor: 'yellow'
+        helloColor: 'yellow',
+        helloPhoto: 'https://static.wikia.nocookie.net/simpsons/images/8/84/Ned_Flanders.png/revision/latest?cb=20201222215900'
     }
 }) 
